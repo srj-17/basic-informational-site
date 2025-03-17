b9d7bde63d69e6079ae6e855553e25628ac03033
srj-17 <saugatrijal17@gmail.com>
2025-03-17 09:53:28 +0545

    feat: Create a nodejs server

---
+++ index.js
@@ -0,0 +1,63 @@
+const http = require("node:http");
+const fs = require("fs/promises");
+
+http
+  .createServer((request, response) => {
+    switch (request.url) {
+      case "/":
+      case "/index.html": {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("index.html", { encoding: "utf8" })
+          .then((data) => {
+            console.log(data);
+            response.write(data);
+            response.end();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+        break;
+      }
+
+      case "/about": {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("about.html", { encoding: "utf8" })
+          .then((data) => {
+            console.log(data);
+            response.write(data);
+            response.end();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+        break;
+      }
+
+      case "/contact-me": {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("contact-me.html", { encoding: "utf8" })
+          .then((data) => {
+            console.log(data);
+            response.write(data);
+            response.end();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+        break;
+      }
+
+      default: {
+        response.writeHead(404, { "Content-Type": "text/html" });
+        fs.readFile("404.html", { encoding: "utf8" })
+          .then((data) => {
+            response.write(data);
+            response.end();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+    }
+  })
+  .listen(8080);
